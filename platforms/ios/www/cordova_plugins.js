fdd4abffb3fbc690b60ceead127c41748d697d89
Jeff Olen <jeff_olen@hotmail.com>
2015-03-15 18:07:46 -0700

    initial commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.rjfun.cordova.iad/www/iAd.js",
+        "id": "com.rjfun.cordova.iad.iAd",
+        "clobbers": [
+            "window.iAd"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.rjfun.cordova.iad": "2.0.3",
+    "com.rjfun.cordova.extension": "1.0.6"
+}
+// BOTTOM OF METADATA
+});
